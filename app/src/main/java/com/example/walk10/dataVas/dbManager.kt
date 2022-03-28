7facf3c368a55d66190edf72b9eb29e02d9670ec
OlgaStroeva <olga,stroevaa@gmail.com>
2022-03-28 12:33:18 +0300

    DB_in_progress

--- app/src/main/java/com/example/walk10/dataVas/dbManager.kt
+++ app/src/main/java/com/example/walk10/dataVas/dbManager.kt
@@ -1,13 +1,15 @@
 package com.example.walk10.dataVas
 
 import com.example.walk10.data.Ad
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class dbManager {
     val db = Firebase.database.getReference("main")
-
+    val auth = Firebase.auth
     fun publishAd(ad : Ad){
-        db.child(ad.key ?: "empty").setValue(ad)
+       if (auth.uid != null)
+           db.child(ad.key ?: "empty").child(auth.uid!!).setValue(ad)
     }
 }
